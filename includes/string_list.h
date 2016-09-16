d9f18b70b8bb16a6a50bd7d9d0cc5272613fb2d1
Nicolas Chevalier <niccheva@student.42.fr>
2016-09-16 09:44:00 +0200

    normed

--- includes/string_list.h
+++ includes/string_list.h
@@ -6,7 +6,7 @@
 /*   By: niccheva <niccheva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/15 21:49:25 by niccheva          #+#    #+#             */
-/*   Updated: 2016/09/16 08:42:12 by niccheva         ###   ########.fr       */
+/*   Updated: 2016/09/16 09:43:23 by niccheva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,8 @@ struct							s_string_list
 t_string_list					*create_string_list(const char *s);
 void							delete_string_list(t_string_list *list);
 void							delete_string_list_entry(t_string_list *list);
-void							string_list_add(t_string_list *list, t_string_list *new);
+void							string_list_add(t_string_list *list,
+												t_string_list *new);
 size_t							string_list_size(const t_string_list *list);
 
 #endif
--- sources/string_list/delete_string_list.c
+++ sources/string_list/delete_string_list.c
@@ -6,7 +6,7 @@
 /*   By: niccheva <niccheva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/16 08:34:03 by niccheva          #+#    #+#             */
-/*   Updated: 2016/09/16 08:45:27 by niccheva         ###   ########.fr       */
+/*   Updated: 2016/09/16 09:43:51 by niccheva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ void				delete_string_list(t_string_list *list)
 	{
 		while (&(list->list) != list->list.next)
 		{
-			delete_string_list_entry(LIST_FIRST_ENTRY(&(list->list), t_string_list, list));
+			delete_string_list_entry(LIST_FIRST_ENTRY(&(list->list),
+													t_string_list, list));
 		}
 		delete_string_list_entry(list);
 	}
